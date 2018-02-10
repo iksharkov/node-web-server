0676cdea41b04bd5acc740e0e305834c3f86e5d1
iksharkov <i.k.sharkov@gmail.com>
2018-02-10 10:24:34 +0200

    Add a new features in app: projects page

--- server.js
+++ server.js
@@ -53,6 +53,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'projects page'
+    });
+});
+
 app.get('/bad', (req,res) => {
     res.send ({
         errorMassage:'Unable to load page!'  
